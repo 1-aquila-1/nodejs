3837e81f7d5624e6dbabcb44707fd507791affec
1-aquila-1 <aquilla.tavares7@gmail.com>
2023-05-16 19:37:13 -0300

    Revert "02-Modelos, migrações e seeds"
    This reverts commit ce84950f0b2460a0e421cc9576eb71d421a87ada.

--- 3-orm-sequelize-mysql/2-migracoes-e-seed/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-app.get("/teste", (req, res) => res.status(200).send({mensagem: "orm sequelize e mysql"}));
-
-export default app;
